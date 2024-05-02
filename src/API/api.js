410e43347786a45d074bf518f34f188ea541af13
matiasromoli <matiasromoli22@gmail.com>
2024-05-02 12:22:16 -0300

    add

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 export async function editCompletedTask(v, id) {
   try {
-    await fetch(`${import.meta.env.VITE_URL}/completed/${id}`, {
+    await fetch(`https://backend-4r6v.onrender.com/completed/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -13,7 +13,7 @@ export async function editCompletedTask(v, id) {
 }
 export async function editTask(id, editTask) {
   try {
-    const res = await fetch(`${import.meta.env.VITE_URL}/edit/${id}`, {
+    const res = await fetch(`https://backend-4r6v.onrender.com/edit/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -27,7 +27,7 @@ export async function editTask(id, editTask) {
 }
 export async function showTaskCompleted() {
   try {
-    const res = await fetch(`${import.meta.env.VITE_URL}/task/completed`);
+    const res = await fetch("https://backend-4r6v.onrender.com/task/completed");
     return await res.json();
   } catch (error) {
     return error;
@@ -35,7 +35,7 @@ export async function showTaskCompleted() {
 }
 export async function saveTask(newTask) {
   try {
-    const response = await fetch(`${import.meta.env.VITE_URL}/task`, {
+    const response = await fetch("https://backend-4r6v.onrender.com/task", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -50,7 +50,7 @@ export async function saveTask(newTask) {
 }
 export async function deleteTask(id) {
   try {
-    const res = await fetch(`${import.meta.env.VITE_URL}/delete/${id}`, {
+    const res = await fetch(`https://backend-4r6v.onrender.com/delete/${id}`, {
       method: "DELETE",
     });
     return res;
@@ -59,6 +59,6 @@ export async function deleteTask(id) {
   }
 }
 export async function showTask() {
-  const response = await fetch(`${import.meta.env.VITE_URL}/task`);
+  const response = await fetch("https://backend-4r6v.onrender.com/task");
   return await response.json();
 }
