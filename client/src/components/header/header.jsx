e6f24c26fa018d5e6eb8df794708232ffcfc0efb
Matias Rómoli <122396770+matias-romoli@users.noreply.github.com>
2024-07-17 18:38:13 -0300

    Update header.jsx

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -14,9 +14,11 @@ function Header() {
 
   return (
     <header className="flex">
-      <div className={`logo-${colors}`}>
-        <ion-icon name="logo-github"></ion-icon>
-      </div>
+ <div className={`logo-${colors}`}>
+  <a href="https://github.com/matias-romoli/task">
+    <ion-icon name="logo-github"></ion-icon>
+  </a>
+</div>
       <div className={`colors-${colors} flex`}>
         <span onClick={onColorDark}>
           <ion-icon name="moon-outline"></ion-icon>
